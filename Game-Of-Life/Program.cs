67c48811ebc15da169f74899c540a09c69026e3a
slash-n-stash <animesh_mishra19@hotmail.com>
2015-08-13 16:19:47 +0530

    Create Program.cs

---
+++ Game-Of-Life/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Of_Life
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
